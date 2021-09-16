54cda49661f86da197b25b50d5b4b8b509d6b9b9
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-16 16:11:32 -0600

    Add saveToStorage method and start on deleteFromStorage

--- Idea.js
+++ Idea.js
@@ -1,3 +1,5 @@
+// var ideas = [];
+
 class Idea {
   constructor(title, body) {
     this.id = Date.now();
@@ -5,4 +7,20 @@ class Idea {
     this.body = body;
     this.star = false;
   }
+
+saveToStorage(ideas) {
+  var stringifiedIdeas = JSON.stringify(ideas);
+  localStorage.setItem('ideas', stringifiedIdeas);
 }
+// deleteFromStorage
+deleteFromStorage(retrieveIdea) {
+  var retrievedIdea = localStorage.getItem('idea');
+  //this specific idea's identity will need to be handled in main.js, when invoked/found this param will be passed into this function
+  localStorage.removeItem(retrievedIdea);
+  var parsedIdea = JSON.parse(retrievedIdea);
+}
+// updateIdea (should update the idea’s starred state)
+
+
+}
+//no DOM manipulation in classes
