1da64ab415887731a6a56e7333545a3fb240cf60
edward krupicka <edwardkrupicka@gmail.com>
2021-09-17 21:09:48 -0600

    Add remove from local storage method to.

--- Idea.js
+++ Idea.js
@@ -10,13 +10,20 @@ class Idea {
 
 saveToStorage(ideas) {
   var stringifiedIdeas = JSON.stringify(ideas);
-  localStorage.setItem('ideas', stringifiedIdeas);
+  localStorage.setItem('stringifiedIdeas', stringifiedIdeas);
 }
 // deleteFromStorage
 deleteFromStorage() {
-  var retrievedIdea = localStorage.getItem('idea');
+  var retrievedIdeas = localStorage.getItem('stringifiedIdeas');
+  var parsedIdeas = JSON.parse(retrievedIdeas);
+  for (var i = 0; i < parsedIdeas.length; i ++) {
+    if (this.id === parsedIdeas[i].id) {
+      parsedIdeas.splice(i, 1);
+    }
+  }
+  this.saveToStorage(parsedIdeas);
   //this specific idea's identity will need to be handled in main.js, when invoked/found this param will be passed into this function
-  localStorage.removeItem(retrievedIdea);
+  // localStorage.removeItem(retrievedIdeas);
   // var parsedIdea = JSON.parse(retrievedIdea);
 }
 // updateIdea (should update the idea’s starred state)
