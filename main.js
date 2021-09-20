9d742b1d7e82cfa035bb88b74cdfbdd04805758c
Ivonne Hernandez <ivonne702@gmail.com>
2021-09-20 14:45:51 -0600

    Show a comment form when the Add icon on an idea card is clicked

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ var ideaCardGrid = document.querySelector('.idea-container');
 var deleteButton = document.querySelector('.delete-btn');
 var searchBox = document.querySelector('#search-box');
 var showStarredIdeasButton = document.querySelector('#show-starred-ideas-button');
-
+var titleLabel = document.querySelector('.title-label');
+var bodyLabel = document.querySelector('.body-label');
 //Event Listeners
 saveButton.addEventListener('click', createIdeaCard);
 ideaCardGrid.addEventListener('click', handleIdeaCardGridClick);
 titleInput.addEventListener('keyup', validateUserInput);
 bodyInput.addEventListener('keyup', validateUserInput);
 searchBox.addEventListener('keyup', activeSearchFilter);
-showStarredIdeasButton.addEventListener('click', showStarredIdeas);//
+showStarredIdeasButton.addEventListener('click', showStarredIdeas);
 
 var ideas = [];
+var comments = [];//
 persistOnPageLoad();
 
 //Event Handlers
@@ -24,8 +26,9 @@ function createIdeaCard(event) {
   var userBody = bodyInput.value;
   var id = Date.now();
   var star = false;
+  var comments = [];
 
-  var savedIdea = new Idea(userTitle, userBody, id, star);
+  var savedIdea = new Idea(userTitle, userBody, id, star, comments);
   ideas.push(savedIdea);
   displayCards();
   savedIdea.saveToStorage(ideas);
@@ -70,22 +73,25 @@ function generateInnerHTML(idea) {
   } else {
     starSource = "./assets/star.svg";
   }
-  return `<div class="box-container" id="${idea.id}">
-    <div class="box-header-container">
-      <input class="star-btn" type="image" name="star button" src="${starSource}" alt="picture-of-a-star">
-      <input class="delete-btn" type="image" name="delete button" src="./assets/delete.svg" alt="picture-of-an-x">
-    </div>
-    <div class="title-body-container">
-      <label class="idea-title">${idea.title}</label>
-      <div class="idea-body-container">
-        <p class="idea-body">${idea.body}</p>
-      </div>
-    </div>
-    <div class="box-footer-container">
-      <button class="comment-btn"><img src="./assets/comment.svg"></button>
-      <label class="comment-label">Comment</label>
-    </div>
-  </div>`;
+  return `<article class="box-container" id="${idea.id}">
+            <div class="box-header-container">
+              <input class="star-btn" type="image" name="star button" src="${starSource}" alt="picture-of-a-star">
+              <input class="delete-btn" type="image" name="delete button" src="./assets/delete.svg" alt="picture-of-an-x">
+            </div>
+            <div class="title-body-container">
+              <label class="idea-title">${idea.title}</label>
+              <div class="idea-body-container">
+                <p class="idea-body">${idea.body}</p>
+              </div>
+            </div>
+            <div class="box-footer-container">
+              <input class="comment-btn" type="image" name="comment button" src="./assets/comment.svg" alt="picture-of-a-plus-sign">
+              <label class="comment-label">Comment</label>
+                <article class="comment">
+                  <p class="idea-comment">${idea.comments}</p>
+                </article>
+            </div>
+          </article>`;
 }
 
 function deleteIdeaCard(ideaId) {
@@ -117,6 +123,10 @@ function handleIdeaCardGridClick(event) {
     favoriteIdeaCard(ideaId);
   }
 
+  if (event.target.classList.contains('comment-btn')) {
+    displayComments(ideaId);
+  }
+
   displayCards();
 }
 
@@ -129,8 +139,9 @@ function persistOnPageLoad() {
       var body = parsedIdeasFromStorage[i].body;
       var id = parsedIdeasFromStorage[i].id;
       var star = parsedIdeasFromStorage[i].star;
+      var comments = parsedIdeasFromStorage[i].comments;//
 
-      var reinstantiatedIdea = new Idea(title, body, id, star);
+      var reinstantiatedIdea = new Idea(title, body, id, star, []);//
       ideas.push(reinstantiatedIdea);
     }
     displayCards();
@@ -160,4 +171,20 @@ function showStarredIdeas() {
     showStarredIdeasButton.innerText = `Show Starred Ideas`;
   }
   displayCards();
+}
+
+function displayComments(ideaId) {
+  // for (var i = 0; i < ideas.length; i ++) {
+  //   if (ideaId === ideas[i].id) {
+  //     var comment = new Comment(
+  //   }
+  // }
+  
+  showCommentForm();
+}
+
+function showCommentForm() {
+  titleInput.classList.add('hidden');
+  titleLabel.classList.add('hidden');
+  bodyLabel.innerText = `Add a comment`;
 }
